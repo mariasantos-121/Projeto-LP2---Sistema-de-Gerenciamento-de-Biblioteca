2bc807517ed46a6a36f2952de768d01b18077062
laribarreto <larissa.barreto.106@ufrn.edu.br>
2025-11-08 22:21:39 -0300

    Update Leitor.java

--- Biblioteca/src/pessoa/Leitor.java
+++ Biblioteca/src/pessoa/Leitor.java
@@ -1,9 +1,19 @@
 package pessoa;
 
 public class Leitor extends Pessoa {
-    //construtor
-    public Leitor(String nome) {
-        super(nome); // chama o construtor da classe pessoa.Pessoa
+    private static int contadorLeitor = 0;
+    private String cpf;
+
+    public Leitor(String nome, String cpf) {
+        super(nome, contadorLeitor++);
+        this.cpf = cpf;
     }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
